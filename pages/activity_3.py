13640db8a311a8f3fe31164006166e78263c0ea0
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-08 11:18:40 +0800

    files

--- pages/activity_3.py
+++ pages/activity_3.py
@@ -75,22 +75,22 @@ def main():
     
     img = read_image("flower.jpg")
     uploaded_file = st.file_uploader("flower.jpg")
-if uploaded_file is not None:
+    if uploaded_file is not None:
     # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
 
     # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
 
     # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+        string_data = stringio.read()
+        st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
 
     translated_img_ = translation_img(img)
     rotated_img_ = rotation_img(img)
