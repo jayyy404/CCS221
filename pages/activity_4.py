d8dadde9ca6bd4f012303a627d353c3d5ff69e0a
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-09 22:07:37 +0800

    files

--- pages/activity_4.py
+++ pages/activity_4.py
@@ -1,8 +1,3 @@
-#Group 4
-#Jed Andrew Del Rosario
-#John Paul T. Sapasap
-#Marc Joshua Esceuta
-
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -12,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from scipy.spatial import Delaunay
 
-def plt_basic_object_(points, counter):
+def plt_basic_object_(points, title):
     tri = Delaunay(points).convex_hull
 
     fig = plt.figure(figsize=(8,8))
@@ -22,42 +17,24 @@ def plt_basic_object_(points, counter):
     ax.set_xlim3d(-10, 10)
     ax.set_ylim3d(-10, 10)
     ax.set_zlim3d(-10, 10)
-    if (counter == 1):
-        plt.title("Pyramid")
-    elif (counter == 2):
-        plt.title("Heart")              #branching statement to show title for each shapes
-    elif (counter == 3):
-        plt.title("Diamond")
+    plt.title(title)
 
-    plt.show()
+    return fig
 
-def _pyramid_(bottom_center=(0, 0, 0)):            #function for the pyramid shape
+def _pyramid_(bottom_center=(0, 0, 0)):
     bottom_center = np.array(bottom_center) 
 
     points = np.vstack([
-    bottom_center + [-3, -3, 0],
-    bottom_center + [-3, +3, 0],
-    bottom_center + [+3, -3, 0],
-    bottom_center + [+3, +3, 0],
-    bottom_center + [0, 0, +5]
+        bottom_center + [-3, -3, 0],
+        bottom_center + [-3, +3, 0],
+        bottom_center + [+3, -3, 0],
+        bottom_center + [+3, +3, 0],
+        bottom_center + [0, 0, +5]
     ])
 
     return points
 
-init_pyramid = _pyramid_(bottom_center=(0,0,0))
-points_pyramid2 = tf.constant(init_pyramid, dtype=tf.float32)
-counter = 1
-plt_basic_object_(init_pyramid, counter)
-x = int(input("Enter for x: "))
-y = int(input("Enter for y: "))                 #prompt the user to enter new values for x, y, and z
-z = int(input("Enter for z: "))
-translation = tf.constant([x, y, z], dtype=tf.float32)
-
-translated_points = points_pyramid2 + translation
-
-plt_basic_object_(translated_points.numpy(), counter)
-
-def _heart_(bottom_center = (0, 0, 0)):                     #function for heart shape
+def _heart_(bottom_center=(0, 0, 0)):
     bottom_center = np.array(bottom_center)
     points = np.vstack([
         bottom_center + [+1.5, -1, +3.5],
@@ -73,45 +50,31 @@ def _heart_(bottom_center = (0, 0, 0)):                     #function for heart 
     ])
     return points
 
-init_heart = _heart_(bottom_center=(0,0,0))
-points_heart = tf.constant(init_heart, dtype=tf.float32)
-counter = 2
-plt_basic_object_(init_heart, counter)
-
-x = int(input("Enter for x: "))
-y = int(input("Enter for y: "))
-z = int(input("Enter for z: "))
-
-translation = tf.constant([x, y, z], dtype=tf.float32)
-
-translated_points_heart = points_heart + translation
-
-plt_basic_object_(translated_points_heart.numpy(), counter)
-
-def _diamond_(bottom_center=(0, 0, 0)):             #fucntion for diamond shape
+def _diamond_(bottom_center=(0, 0, 0)):
     bottom_center = np.array(bottom_center)
 
     points = np.vstack([
-    bottom_center + [+2.5, +2.5, 0],
-    bottom_center + [-2.5, +2.5, 0],
-    bottom_center + [+2.5, -2.5, 0],
-    bottom_center + [-2.5, -2.5, 0],
-    bottom_center + [0, 0, 5],
-    bottom_center + [0, 0, -5]
+        bottom_center + [+2.5, +2.5, 0],
+        bottom_center + [-2.5, +2.5, 0],
+        bottom_center + [+2.5, -2.5, 0],
+        bottom_center + [-2.5, -2.5, 0],
+        bottom_center + [0, 0, 5],
+        bottom_center + [0, 0, -5]
     ])
     return points
 
-init_pyramid = _diamond_(bottom_center=(0,0,0))
-points_pyramid = tf.constant(init_pyramid, dtype=tf.float32)
-counter = 3
-plt_basic_object_(init_pyramid, counter)
+# Sidebar title and subtitle
+st.sidebar.title("3D Object Translator")
+st.sidebar.subheader("Select an object to translate:")
 
-x = int(input("Enter for x: "))
-y = int(input("Enter for y: "))
-z = int(input("Enter for z: "))
+# Sidebar object selection
+object_choice = st.sidebar.selectbox(
+    "",
+    options=["Pyramid", "Heart", "Diamond"]
+)
 
-translation = tf.constant([x, y, z], dtype=tf.float32)
+# Sidebar translation input
+x = st.sidebar.number_input("Translate in x-axis:", value=0, step=1)
+y = st.sidebar.number_input("Translate in y-axis:", value=0, step=1)
+z = st.sidebar.number_input("Translate in z-axis:", value=0, step=1)
 
-translated_points_pyramid = points_pyramid + translation
-
-plt_basic_object_(translated_points_pyramid.numpy(), counter)
