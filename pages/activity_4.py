1f8d08d022939eb3a6d6f4b1153322562a7a47c3
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-10 00:00:49 +0800

    files

--- pages/activity_4.py
+++ pages/activity_4.py
@@ -1,20 +1,87 @@
-import streamlit as st
-import matplotlib.pyplot as plt
+#Group 4
+#Members : John Paul Sapasap
+         # Jed Andrew Del Rosario
+         # Marc Joshua Escueta
 import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import streamlit as st
 
-# create some sample data
-x = np.arange(0, 10, 0.1)
-y = np.sin(x)
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from scipy.spatial import Delaunay
 
-# create a matplotlib figure
-fig, ax = plt.subplots()
-ax.plot(x, y)
+def plt_basic_object_(points, counter):
+    tri = Delaunay(points).convex_hull
 
-# display the figure in Streamlit
-st.pyplot(fig)
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111, projection='3d')
+    S = ax.plot_trisurf(points[:,0], points[:,1], points[:,2],triangles=tri,shade=True, cmap=cm.seismic,lw=0.5)
 
+    ax.set_xlim3d(-10, 10)
+    ax.set_ylim3d(-10, 10)
+    ax.set_zlim3d(-10, 10)
+    if (counter == 1):
+        plt.title("Pyramid")
+    elif (counter == 2):
+        plt.title("Heart")
+    elif (counter == 3):
+        plt.title("Diamond")
 
+    return fig
 
+def _pyramid_(bottom_center=(0, 0, 0)):
+    bottom_center = np.array(bottom_center) 
 
+    points = np.vstack([
+    bottom_center + [-3, -3, 0],
+    bottom_center + [-3, +3, 0],
+    bottom_center + [+3, -3, 0],
+    bottom_center + [+3, +3, 0],
+    bottom_center + [0, 0, +5]
+    ])
 
+    return points
 
+init_pyramid = _pyramid_(bottom_center=(0,0,0))
+points_pyramid2 = tf.constant(init_pyramid, dtype=tf.float32)
+counter = 1
+fig1 = plt_basic_object_(init_pyramid, counter)
+st.pyplot(fig1)
+
+x = int(input("Enter for x: "))
+y = int(input("Enter for y: "))
+z = int(input("Enter for z: "))
+
+translation = tf.constant([x, y, z], dtype=tf.float32)
+
+translated_points = points_pyramid2 + translation
+
+fig2 = plt_basic_object_(translated_points.numpy(), counter)
+st.pyplot(fig2)
+
+def _heart_(bottom_center = (0, 0, 0)):
+    bottom_center = np.array(bottom_center)
+    points = np.vstack([
+        bottom_center + [+1.5, -1, +3.5],
+        bottom_center + [+1.5, +1, +3.5],
+        bottom_center + [-1.5, -1, +3.5],
+        bottom_center + [-1.5, +1, +3.5],        
+        bottom_center + [0, +1, +3],
+        bottom_center + [0, -1, +2],
+        bottom_center + [+3, 0, +2],
+        bottom_center + [-3, 0, +2],
+        bottom_center + [0, 1, -2],
+        bottom_center + [0, -1, -2]
+    ])
+    return points
+
+init_heart = _heart_(bottom_center=(0,0,0))
+points_heart = tf.constant(init_heart, dtype=tf.float32)
+counter = 2
+fig3 = plt_basic_object_(init_heart, counter)
+st.pyplot(fig3)
+
+x = int(input("Enter for x: "))
+y = int(input("Enter for y: "))
+z = int(input("Enter for z: "))
