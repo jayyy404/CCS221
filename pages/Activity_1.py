312465808ff4af161c1afbf5c8bdbde00a71298a
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-11 21:43:37 +0800

    files

--- pages/Activity_1.py
+++ pages/Activity_1.py
@@ -7,152 +7,88 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 
-plt.title("DDA Line Algorithm")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
 def DDALine(x1, y1, x2, y2, color):
     dx = x2 - x1
     dy = y2 - y1
-
-    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-    Xinc = float(dx / steps)
-    Yinc = float(dy / steps)
-
- 
-    for i in range(0, int(steps + 1)):
-        plt.plot(int(x1), int(y1), color)
-        x1 += Xinc
-        y1 += Yinc
-
-    plt.show()
-
-
-
-#DDA Line with midpoints
-plt.title("DDA line/ Midpoints")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-def DDALine_mpoints(x1, y1, x2, y2, color):
-    dx = x2 - x1
-    dy = y2 - y1
-
-    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
-
-    Xinc = float(dx / steps)
-    Yinc = float(dy / steps)
-
-    xm = (x1 + x2)/2
-    ym = (y1 + y2)/2                                            #These line of codes are the midpoints formula
-    print ("\nX midpoint: ",xm, "\nY midpoint: ",ym)
-    plt.plot(xm,ym, marker = 'o', markerfacecolor = "green")
-
-    for i in range(0, int(steps + 1)):
-        plt.plot(int(x1), int(y1), color)
-        x1 += Xinc
-        y1 += Yinc
-
-    plt.show()                                                  #this let the program show a graph for the users input
-
-#Brasenham lINE
-plt.title("Braseham Line")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-def bres_line(x1,y1,x2,y2):
-    x,y = x1, y1
-    dx = abs(x2 - x1) 
-    dy = abs(y2 - y1) 
-    slope = dy/float(dx)
-
-    if slope > 1:
-        dx, dy = dy, dx
-        x, y = y, x
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-
-    p = 2 * dy - dx
-    xcoords = [x]
-    ycoords = [y]
-
-    for i in range(2, dx):
-        if p > 0:
-            y = y + 1 if y < y2 else y - 1
-            p = p + 2 * (dy - dx)
-        else:
-            p = p + 2 * dy 
-
-        x = x + 1 if x < x2 else x - 1
-        xcoords.append(x)
-        ycoords.append(y)
-
-    plt.plot(xcoords, ycoords)
-    plt.show()
-
-#Brasenham Line wirh Midpoint
-plt.title("Braseham Line/Midpoint")
-plt.xlabel("X Axis")
-plt.ylabel("Y Axis")
-def bres_line_mpoint(x1,y1,x2,y2):
-    x,y = x1, y1
-    dx = abs(x2 - x1) 
-    dy = abs(y2 - y1) 
-    slope = dy/float(dx)
-
-    if slope > 1:
-        dx, dy = dy, dx
-        x, y = y, x
-        x1, y1 = y1, x1
-        x2, y2 = y2, x2
-
-    p = 2 * dy - dx
-    xcoords = [x]
-    ycoords = [y]
-
-    xm = (x1 + x2)/2
-    ym = (y1 + y2)/2                                            #These line of codes are the midpoints formula
-    print ("\nX midpoint: ",xm, "\nY midpoint: ",ym)
-    plt.plot(xm,ym, marker = 'o', markerfacecolor = "green")
-
-    for i in range(2, dx):
-        if p > 0:
-            y = y + 1 if y < y2 else y - 1
-
-            p = p + 2 * (dy - dx)
-        else:
-            p = p + 2 * dy 
-
-        x = x + 1 if x < x2 else x - 1
-        xcoords.append(x)
-        ycoords.append(y)
-
-    plt.plot(xcoords, ycoords)
-    plt.show()
-
-
-draw_funcs = {
-    'DDA Line': DDALine,
-    'DDA Line with midpoints': DDALine_mpoints,
-    'Bresenham Line': bres_line,
-    'Bresenham Line with midpoint': bres_line_mpoint
-}
-
-# Define the Streamlit app
-def main():
-    st.title('Line Drawing App')
-    st.write('Choose a line drawing function:')
     
-    # Create a dropdown menu to let the user choose the drawing function
-    func_name = st.selectbox('Function', list(draw_funcs.keys()))
+    if dx == 0 and dy == 0:
+        # both dx and dy are zero, so there is nothing to draw
+        st.warning("Starting and ending points are the same.")
+        return
 
-    # Get user's input for the line coordinates
-    x1 = st.number_input('X1', value=0)
-    y1 = st.number_input('Y1', value=0)
-    x2 = st.number_input('X2', value=0)
-    y2 = st.number_input('Y2', value=0)
-    color = st.text_input('Color', default='r.')
+    steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
 
-    # Draw the line using the selected function
-    if st.button('Draw'):
-        fig, ax = plt.subplots()
-        draw_funcs[func_name](x1, y1, x2, y2, color)
-        st.pyplot(fig)
+    Xinc = float(dx / steps)
+    Yinc = float(dy / steps)
+
+    x = x1
+    y = y1
+    fig, ax = plt.subplots()
+    for i in range(0, int(steps+1)):
+        ax.plot(int(x), int(y), color)
+        x += Xinc
+        y += Yinc
+
+    ax.set_xlabel("X-Axis")
+    ax.set_ylabel("Y-Axis")
+    ax.set_title("DDA Algorithm")
+    st.pyplot(fig)
+
+
+def BresenhamLine(x1, y1, x2, y2):
+    x, y = x1, y1
+    dx = abs(x2 - x1)
+    dy = abs(y2 - y1)
+    gradient = dy / float(dx)
+
+    if gradient > 1:
+        dx, dy = dy, dx
+        x, y = y, x
+        x1, y1 = y1, x1
+        x2, y2 = y2, x2
+
+    p = 2 * dy - dx
+    xcoordinates = [x]
+    ycoordinates = [y]
+
+    for k in range(2, dx+2):
+        if p > 0:
+            y = y + 1 if y < y2 else y - 1
+            p = p + 2 * (dy - dx)
+        else:
+            p = p + 2 * dy
+
+        x = x + 1 if x < x2 else x - 1
+        xcoordinates.append(x)
+        ycoordinates.append(y)
+
+    midX = (x1 + x2) // 2
+    midY = (y1 + y2) // 2
+    st.write("Midpoint of the line is at ({}, {})".format(midX, midY))
+
+    plt.title("Bresenham Algorithm")
+    plt.xlabel("X Axis")
+    plt.ylabel("Y Axis")
+    plt.scatter(xcoordinates, ycoordinates, color='red')
+    st.pyplot()
+
+def main():
+    st.sidebar.title("Select Algorithm")
+    algorithm = st.sidebar.selectbox("Select Algorithm", ("DDA", "Bresenham"))
+
+    x1 = st.sidebar.number_input("Enter the Starting point of x:")
+    y1 = st.sidebar.number_input("Enter the Starting point of y:")
+    x2 = st.sidebar.number_input("Enter the end point of x:")
+    y2 = st.sidebar.number_input("Enter the end point of y:")
+
+    if algorithm == "DDA":
+        color = st.sidebar.text_input("Enter the color for DDA Line (e.g. 'r.', 'g--', etc.):")
+        DDALine(x1, y1, x2, y2, color)
+    elif algorithm == "Bresenham":
+        BresenhamLine(x1, y1, x2, y2)
+
+main()
