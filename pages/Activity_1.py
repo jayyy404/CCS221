3b65e19141275f504c91b26cb045f43b433af002
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-11 21:48:15 +0800

    files

--- pages/Activity_1.py
+++ pages/Activity_1.py
@@ -86,7 +86,7 @@ def main():
     y2 = st.sidebar.number_input("Enter the end point of y:")
 
     if algorithm == "DDA":
-        color = red
+        color = "r."
         DDALine(x1, y1, x2, y2, color)
     elif algorithm == "Bresenham":
         BresenhamLine(x1, y1, x2, y2)
