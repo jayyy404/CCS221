90f5e34a53a827d7941053f98297211996f1a92a
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-13 21:30:26 +0800

    files

--- pages/Activity_1.py
+++ pages/Activity_1.py
@@ -59,7 +59,7 @@ def bres_line(x1,y1,x2,y2,color):
     ycoords = [y]
 
     fig,ax = plt.subplots()
-    for i in range(1, int(dx)):
+    for i in range(1, dx):
         if p > 0:
             y = y + 1 if y < y2 else y - 1
             p = p + 2 * (dy - dx)
