650eb1c9d6452930b9646e9eedc62f2c891e0e1c
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-14 15:59:06 +0800

    files

--- pages/activity_3.py
+++ pages/activity_3.py
@@ -81,8 +81,6 @@ def main():
         bytes_data = uploaded_file.read()
         nparr = np.frombuffer(bytes_data, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    else:
-        img = read_image("flower.jpg")
 
     # perform transformations
     translated_img_ = translation_img(img)
