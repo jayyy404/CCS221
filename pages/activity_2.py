0bcd04de33ece792e6fad50cd511add472243f2d
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-14 15:14:28 +0800

    files

--- pages/activity_2.py
+++ pages/activity_2.py
@@ -4,41 +4,20 @@ import streamlit as st
 
 two_d_arr = np.array([[1,0,1], [0,1,0],[1,0,1]])
 
-def change(x, y, color, direction):
-    if direction == '1':
-        two_d_arr[x][y] = color
-        if x > 0:
-            change(x-1, y, color, direction)
-    elif direction == '2':
-        two_d_arr[x][y] = color
-        if x < two_d_arr.shape[0]-1:
-            change(x+1, y, color, direction)
-    elif direction == '3':
-        two_d_arr[x][y] = color
-        if y > 0:
-            change(x, y-1, color, direction)
-    elif direction == '4':
-        two_d_arr[x][y] = color
-        if y < two_d_arr.shape[1]-1:
-            change(x, y+1, color, direction)
-    else:
-        print('Please Try again')
-    
-    img = plt.imshow(two_d_arr, interpolation='none', cmap='Pastel2')
+def change(x, y, color):
+    two_d_arr[x][y] = color
+    fig, ax = plt.subplots()
+    img = ax.imshow(two_d_arr, interpolation='none', cmap='Reds_r')
     img.set_clim([0,50])
-    plt.colorbar()
-    return img
+    plt.colorbar(img)
+    st.pyplot(fig)
 
 def main():
-    st.title("Color Mapping")
-    x_coordinates = st.number_input("X coordinates:", min_value=0, max_value=2, step=1)
-    y_coordinates = st.number_input("Y coordinates:", min_value=0, max_value=2, step=1)
-    colorvalue = st.selectbox("Select a Color Value (1-50)", options=list(range(1, 51)))
-    direction = st.selectbox("Direction", options=["1 for up", "2 for down", "3 for left", "4 for right"])
-    direction_mapping = {"1 for up": "1", "2 for down": "2", "3 for left": "3", "4 for right": "4"}
-    direction_code = direction_mapping[direction]
-    img = change(x_coordinates, y_coordinates, colorvalue, direction_code)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot()
+    two_d_arr = np.array([[1,0,1], [0,1,0],[1,0,1]])
+    for i in range(3):
+        x_val = st.number_input("Input the X coordinate (row 0,1,2) that are available:", 0, 2, 0, key=f"x_val_{i}")
+        y_val = st.number_input("Input the Y coordinate (column 0,1,2) that are available:", 0, 2, 0, key=f"y_val_{i}")
+        c_val = st.number_input("Enter a Color Value from (1-50):", 1, 50, 1, key=f"c_val_{i}")
+        change(x_val, y_val, c_val)
 
 main()
