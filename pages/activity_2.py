5358a8a9a23eedcd655dda821d7a274492c1159a
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-23 10:55:51 +0800

    files

--- pages/activity_2.py
+++ pages/activity_2.py
@@ -22,9 +22,9 @@ def change(x, y, color):
 def main():
     two_d_arr = np.array([[1,0,1], [0,1,0],[1,0,1]])
     for i in range(2):
-        x= st.number_input("Input the X coordinate (row 0,1,2) that are available:", 0, 2, 0, key=f"x_val_{i}")
-        y = st.number_input("Input the Y coordinate (column 0,1,2) that are available:", 0, 2, 0, key=f"y_val_{i}")
-        color = st.number_input("Enter a Color Value from (1-50):", 1, 50, 1, key=f"c_val_{i}")
+        x= st.sidebar.number_input("Input the X coordinate (row 0,1,2) that are available:", 0, 2, 0, key=f"x_val_{i}")
+        y = st.sidebar.number_input("Input the Y coordinate (column 0,1,2) that are available:", 0, 2, 0, key=f"y_val_{i}")
+        color = st.sidebar.number_input("Enter a Color Value from (1-50):", 1, 50, 1, key=f"c_val_{i}")
         change(x,y,color)
 
 main()
