f946836c187968593141aec4b217f79dc9cff059
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-23 11:00:37 +0800

    files

--- pages/QUIZZZZ.py
+++ pages/QUIZZZZ.py
@@ -18,9 +18,9 @@ def translate_image(img, bx, by):
     m = np.float32([[1, 0, bx], [0, 1, by]])
     return cv2.warpAffine(img, m, (cols, rows))
 
-def new_translated(img, bx_old, by_old, tx, ty):
-    bx_new = bx_old + tx *10
-    by_new = by_old + ty*10
+def new_translated(img, bx_old, by_old, tx, ty,x,y):
+    bx_new = bx_old + tx *x
+    by_new = by_old + ty*y
     return translate_image(img, bx_new, by_new)
 
 def main():
@@ -37,11 +37,14 @@ def main():
     translated_imgs = []
     for bx, by in zip(range(2, 7), range(5, 10)):
         translated_imgs.append(translate_image(img, bx, by))
+        
+    x = st.sidebar.number_input("Enter the value of x:")
+    y = st.sidebar.number_input("Enter the value of y:")
 
     # IMAGES WITH NEW VALUES
     modified_imgs = []
     for bx_old, by_old, tx, ty in [(2, 5, 6, 2), (3, 6, 7, 3), (4, 7, 8, 4), (5, 8, 9, 5), (6, 9, 10, 3)]:
-        modified_imgs.append(new_translated(img, bx_old, by_old, tx, ty))
+        modified_imgs.append(new_translated(img, bx_old, by_old, tx, ty,x,y))
 
     # Display images using st.pyplot()
     for i in range(5):
