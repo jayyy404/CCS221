9b4187ad7cf5b81f3b1dc8e88296832bf3bfa278
jayyy404 <johnpaul.sapasap@wvsu.edu.ph>
2023-03-26 18:06:28 +0800

    files

--- pages/Activity_3___Part_2.py
+++ pages/Activity_3___Part_2.py
@@ -52,8 +52,7 @@ def main():
         bytes_data = uploaded_file.read()
         nparr = np.frombuffer(bytes_data, np.uint8)
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    else:
-        img = read_image("flower.jpg")
+
         
     st.image(img, use_column_width=True, caption="Original Image")
     
